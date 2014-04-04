60c5e3046e6640a67660e8093183f4c7046b0690
N3ggro <n3ggro@gmail.com>
2014-04-04 00:11:32 -0300

    borrado funciona, todo perfect

--- wea.sh
+++ wea.sh
@@ -7,16 +7,21 @@ make
 gcc -Wall Main.o
 clear
 
-echo "rojo"
+echo "i rojo"
 ./a.out -i rojo 0 0 0
-echo "azul"
+echo "i azul"
 ./a.out -i azul 0 0 0
-echo "rojo"
+echo "i rojo"
 ./a.out -i rojo 0 0 0
-echo "verde"
+echo "i verde"
 ./a.out -i verde 0 0 0
-
-
-
-
-
+echo "d azul"
+./a.out -d azul
+echo "d verde"
+./a.out -d verde
+echo "d verde"
+./a.out -d verde
+echo "d rojo"
+./a.out -d rojo
+echo "i amarillo"
+./a.out -i amarillo 1 2 3
